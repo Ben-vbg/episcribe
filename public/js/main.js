5b1d8be041af7a2407380bae02720fce5bde4a69
Verboogen <38947986+Ben-vbg@users.noreply.github.com>
2021-11-29 22:17:56 +0100

    up

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,25 @@ $(document).ready(()=> {
     }else{
       $('nav').removeClass('blacked');
     }
-  })
+  });
+
+
+
+  fetch('https://www.episcribe.be/cockpit/api/collections/get/services?token=b2aff9e33dda2ee179beb889fb0c86')
+  .then(res=>res.json())
+  .then(function(response) {
+    printData(response);
+  });
+
+  function printData(r){
+    $('[data-c]').each(function(){
+      let id = $(this).data('c');
+      let val = r.entries[0][id];
+      console.log(val);
+      $(this).text(val);
+    });
+  }
+
 
 });
+
