4c78fca5d98c5838c32bfd7ce5202b98f618f09b
Verboogen <38947986+Ben-vbg@users.noreply.github.com>
2022-01-10 17:00:57 +0100

    update

--- public/js/main.js
+++ public/js/main.js
@@ -25,11 +25,29 @@ $(document).ready(()=> {
     });
   }
 
-  let g__id = $('body').attr('id');
+  // Configuration
+  fetch(`
+  https://www.episcribe.be/cockpit/api/collections/get/configuration?token=b2aff9e33dda2ee179beb889fb0c86
+  `)
+  .then(res=>res.json())
+  .then(function(response) {
+    printPage(response);
+  });
 
+  // Posts
+  fetch(`
+  https://www.episcribe.be/cockpit/api/collections/get/posts?token=b2aff9e33dda2ee179beb889fb0c86
+  `)
+  .then(res=>res.json())
+  .then(function(response) {
+    printShortPosts(response);
+  });
+
+
+  
+  let g__id = $('body').attr('id');
   if(g__id){
     console.log('g__id find!')
-
     fetch(`
     https://www.episcribe.be/cockpit/api/collections/get/${g__id}?token=b2aff9e33dda2ee179beb889fb0c86
     `)
@@ -37,20 +55,46 @@ $(document).ready(()=> {
     .then(function(response) {
       printPage(response);
     });
-  
-    function printPage(r){
-      $('[data-c]').each(function(){
-        let id = $(this).data('c');
-        let val = r.entries[0][id];
-        $(this).html(val);
-      });
-    }
-    
   }else{
     console.log('no g__id!')
   }
 
 
+  function printPage(r){
+    $('[data-c]').each(function(){
+      let id = $(this).data('c');
+      let val = r.entries[0][id];
+      $(this).html(val);
+    });
+  }
+
+  function printShortPosts(r){
+    
+    $('[data-shortPosts]').each(function(){
+
+      let shortPosts = "";
+
+      for (let i = 0; i < r.entries.length; i++) {
+        console.log(r.entries[i])
+        if(r.entries[i].titre && r.entries[i].corp){
+
+          shortPosts += `
+          <div class="grid__col blog__short">
+            <h3>${r.entries[i].titre}</h3>
+            <p>
+              ${r.entries[i].chapeau.slice(0,157)}...
+            </p>
+          </div>
+          `;
+        }
+
+      }
+      console.log(shortPosts);
+      $(this).after(shortPosts);
+      $(this).remove();
+
+    });
+  }
 
 
 });
